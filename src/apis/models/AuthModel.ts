ca01b2ce1db6daa3f430a2892c042b35d1a9ab87
DESKTOP-JPSD7TN\ITOS <jwyou@it-os.co.kr>
2024-11-14 15:44:57 +0900

    init project

---
+++ src/apis/models/AuthModel.ts
@@ -0,0 +1,15 @@
+
+export interface AuthExchange{
+    accessKey: string;
+    refreshKey: string;
+    md5hash: string;
+}
+
+export interface RequestAuthToken{
+    id: string;
+    pw: string;
+}
+
+export interface RequestTokenRefresh{
+    refreshToken: string;
+}
